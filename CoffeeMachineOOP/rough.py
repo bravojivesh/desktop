e25bd86840312688d1505daf9cbd61a2a37d2dad
JIVESH HAMAL <bravojivesh@gmail.com>
2023-12-20 22:42:27 +0530

    first commit. no code yet

---
+++ CoffeeMachineOOP/rough.py
@@ -0,0 +1,13 @@
+import turtle
+
+timmy= turtle.Turtle()
+timmy.shape("circle")
+timmy.color ("green")
+timmy.forward(100)
+
+my_screen= turtle.Screen()
+my_screen.exitonclick()
+
+
+# print (timmy)
+# print (my_screen)
